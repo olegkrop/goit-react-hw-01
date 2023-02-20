17e0e9bd27cb6cd106d5fed26ff97de3cf218a1a
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-02-20 20:36:08 +0200

    feat: components Profile & Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
-import Profile from './Profile';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import user from 'mocks/user.json';
+import data from 'mocks/data.json';
 
 export const App = () => {
   return (
@@ -12,7 +15,14 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,43 @@
 import './Profile.css';
+import PropTypes from 'prop-types';
+// import user from '../../mocks/user.json';
 
-export default function Profile() {
+export function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div className="profile">
       <div className="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          className="avatar"
-        />
-        <p className="name">Petra Marica</p>
-        <p className="tag">@pmarica</p>
-        <p className="location">Salvador, Brasil</p>
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">1000</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">2000</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">3000</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
